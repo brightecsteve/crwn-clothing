bec03df769e3dda9d18f34bc3558af18e475d465
Steve Johnson <steve@brightec.co.uk>
2020-06-29 08:13:14 +0100

    updating our app to redirect to home if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,8 @@
+import { UserActionTypes } from './user.types';
+
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 export const setCurrentUser = user => ({
-    type: SET_CURRENT_USER,
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user,
 });
