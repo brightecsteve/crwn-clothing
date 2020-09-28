94e8a43c0d5588baa128fc5b0c964c960206b6c1
Steve Johnson <steve@brightec.co.uk>
2020-09-28 12:28:04 +0100

    master: created utils with addItemToCart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,7 @@ export const toggleCardHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
 
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item,
+});
